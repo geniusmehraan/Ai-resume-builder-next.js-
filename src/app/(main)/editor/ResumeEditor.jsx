49001cccb3dd026d8a3f782894c7b29fab59220f
SHAKIR <shakirsjd@gmail.com>
2024-12-25 09:10:52 +0530

    tried the easer way of handling form means react hook form

--- src/app/(main)/editor/ResumeEditor.jsx
+++ src/app/(main)/editor/ResumeEditor.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import GeneralInform from "./forms/GeneralInform";
 
 
 
@@ -8,9 +9,9 @@ const ResumeEditor = () => {
   return (
     <div className="flex grow flex-col h-full">
 
-<header className="p-2 ">
+<header className=" space-y-1.5 border-b text-center">
         <div className="flex flex-col text-center justify-center p-4">
-          <h1 className="text-2xl font-bold">Design your resume</h1>
+          <h1 className="lg:text-2xl xl:text-2xl md:text-xl font-bold">Design your resume</h1>
           <span className="text-muted-foreground">
             Follow the steps to design your resume. Progress will be saved
             automatically
@@ -18,15 +19,21 @@ const ResumeEditor = () => {
         </div>
       </header>
 
-      <main className="flex h-full w-full">
-        <div className="flex h-full w-full "></div>
+      <main className="relative grow">
+        <div className="absolute top-0 bottom-0 flex w-full ">
+          <div className="p-3 w-full md:w-1/2">
+          <GeneralInform></GeneralInform>
+          </div>
+          <div className="grow border-r"></div>
+          <div className="hidden w-1/2 md:flex">left</div>
+        </div>
       </main>
 
 
      
-      <footer className="w-full border-t p-4">
-        <div className="flex justify-between items-center px-32 gap-2 w-full">
-          <div>
+      <footer className="w-full p-4">
+        <div className="flex  justify-between md:px-4 sm:px-2 lg:px-32 xl:px-38 space-y-1.5 items-center   w-full">
+          <div className="flex gap-2">
             <Button variant={"secondary"}>Prevoius step</Button>
             <Button>Next step</Button>
           </div>
---
+++ src/app/(main)/editor/forms/GeneralInform.tsx
@@ -0,0 +1,63 @@
+import { generalInfoSchema, GeneralInfoValue } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const GeneralInform = () => {
+  const form = useForm<GeneralInfoValue>({
+    resolver: zodResolver(generalInfoSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+  return (
+    <div className="max-w-xl mx-auto space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-xl font-semibold">General Info</h2>
+      </div>
+      <Form {...form}>
+        <form className="space-y-1.5">
+          <FormField
+            control={form.control}
+            name="title"
+            render={(field) => (
+              <FormItem>
+                <FormLabel>Resume name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="My cool Resume" />
+                </FormControl>
+              </FormItem>
+            )}
+          ></FormField>
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={(field) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Resume for my Fantastic job" />
+                </FormControl>
+                <FormDescription>
+                  Describe what this resume is for
+                </FormDescription>
+              </FormItem>
+            )}
+          ></FormField>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default GeneralInform;
