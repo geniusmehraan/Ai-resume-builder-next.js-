c68d306cc83ed0518e4d5a0eb29c5b29b4bbb966
Genuis mehraan <mehdiayaan786@gmail.com>
2025-01-21 11:45:55 +0530

    now use can drag and drop the work expereice

--- src/app/(main)/editor/forms/WorkExperienceForm.tsx
+++ src/app/(main)/editor/forms/WorkExperienceForm.tsx
@@ -7,6 +7,17 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {CSS} from "@dnd-kit/utilities"
+import { arrayMove, SortableContext, sortableKeyboardCoordinates, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { EditorFormProps } from "@/lib/Types";
@@ -15,6 +26,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { GripHorizontal } from "lucide-react";
 import { useEffect } from "react";
 import { useFieldArray, useForm, UseFormReturn } from "react-hook-form";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers"
 
 const WorkExperienceForm = ({ resumeData, setResumeData }: EditorFormProps) => {
   const form = useForm<WorkExperiencesValues>({
@@ -41,10 +53,29 @@ const WorkExperienceForm = ({ resumeData, setResumeData }: EditorFormProps) => {
     return unsubscribe;
   }, [form, resumeData, setResumeData]);
 
-  const { fields, append, remove } = useFieldArray({
+  const { fields, append, remove, move } = useFieldArray({
     control: form.control,
     name: "workExperiences",
   });
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+
+  function handleDragEnd(event:DragEndEvent){
+    const {active,over} = event
+
+    if(over && active.id !== over.id){
+       const oldIndex = fields.findIndex(field=>field.id === active.id)
+       const newIndex = fields.findIndex(field=>field.id===over.id)
+       move(oldIndex,newIndex)
+       return arrayMove(fields, oldIndex, newIndex)
+    }
+  }
 
   return (
     <div className="mx-auto max-w-xl space-y-6">
@@ -52,15 +83,21 @@ const WorkExperienceForm = ({ resumeData, setResumeData }: EditorFormProps) => {
         <h2 className="text-lg font-semibold">Work Experience</h2>
       </div>
       <Form {...form}>
-        <form className="spce-y-3">
+        
+        <form className="space-y-3">
+        <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd} modifiers={[restrictToVerticalAxis]}>
+          <SortableContext strategy={verticalListSortingStrategy} items={fields}>
           {fields.map((field, index) => (
             <WorkExperience
+             id={field.id}
               key={field.id}
               index={index}
               form={form}
               remove={remove}
             />
           ))}
+          </SortableContext>
+          </DndContext>
           <div className="mt-6">
             <Button
               type="button"
@@ -86,48 +123,48 @@ const WorkExperienceForm = ({ resumeData, setResumeData }: EditorFormProps) => {
 export default WorkExperienceForm;
 
 interface WorkExperieneProps {
+  id:string;
   form: UseFormReturn<WorkExperiencesValues>;
   index: number;
   remove: (index: number) => void;
 }
 
-const WorkExperience = ({ form, index, remove }: WorkExperieneProps) => {
+const WorkExperience = ({id, form, index, remove }: WorkExperieneProps) => {
+  const {attributes,listeners,setNodeRef,transform,transition,isDragging} = useSortable({id})
   return (
-    <div className="space-y-3 mb-2 border bg-background p-3">
+    <div className="space-y-3 mb-2 border bg-background p-3" ref={setNodeRef} style={{transform:CSS.Transform.toString(transform),transition,}}>
       <div className="flex justify-between gap-2">
         <span className="font-semibold">Work Experience {index + 1}</span>
-        <GripHorizontal className="cursor-grab"></GripHorizontal>
+        <GripHorizontal className="cursor-grab"   {...attributes} {...listeners}></GripHorizontal>
       </div>
 
-      
-
       <div className="grid grid-cols-2 gap-2">
-      <FormField
-        control={form.control}
-        name={`workExperiences.${index}.position`}
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Work Title</FormLabel>
-            <FormControl>
-              <Input {...field} autoFocus></Input>
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name={`workExperiences.${index}.company`}
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Company</FormLabel>
-            <FormControl>
-              <Input {...field} autoFocus></Input>
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+        <FormField
+          control={form.control}
+          name={`workExperiences.${index}.position`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Work Title</FormLabel>
+              <FormControl>
+                <Input {...field} autoFocus></Input>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={`workExperiences.${index}.company`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Company</FormLabel>
+              <FormControl>
+                <Input {...field} autoFocus></Input>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </div>
 
       <FormField
@@ -137,7 +174,7 @@ const WorkExperience = ({ form, index, remove }: WorkExperieneProps) => {
           <FormItem>
             <FormLabel>Description</FormLabel>
             <FormControl>
-              <Textarea {...field} autoFocus/>
+              <Textarea {...field} autoFocus />
             </FormControl>
             <FormMessage />
           </FormItem>
@@ -157,7 +194,6 @@ const WorkExperience = ({ form, index, remove }: WorkExperieneProps) => {
           </FormItem>
         )}
       />
-      
 
       <FormField
         control={form.control}
@@ -173,8 +209,9 @@ const WorkExperience = ({ form, index, remove }: WorkExperieneProps) => {
         )}
       />
 
-<Button variant={"destructive"} onClick={()=>remove(index)}>Remove</Button>
-
+      <Button variant={"destructive"} onClick={() => remove(index)}>
+        Remove
+      </Button>
     </div>
   );
 };
